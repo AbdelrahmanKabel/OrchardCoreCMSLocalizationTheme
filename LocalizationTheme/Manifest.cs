dac4c66f0d880ddf8f56447378b9e743388f919c
Abdelrahman Kabel <AKabel@cairo.aas.com.sa>
2022-09-29 15:20:12 +0200

    Add project files.

---
+++ LocalizationTheme/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.DisplayManagement.Manifest;
+using OrchardCore.Modules.Manifest;
+
+[assembly: Theme(
+    Name = "Localization Theme",
+    Author = "Abdo Qabel",
+    Website = "https://naseej.com",
+    Version = ManifestConstants.OrchardCoreVersion,
+    Description = "A theme adapted for localization.",
+    Tags = new[] { "Localization", "Blog", "Bootstrap", "Liquid" }
+)]
